b1893903630c2e8d2f4d56610d038d6d2b5dce38
Quynh To Tuan <quynh.totuan@gmail.com>
2017-05-04 12:35:24 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,7 @@
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+  sorted = arr.sort_by do |e|
+    e.chars.map { |letter| alphabet.index(letter) if !alphabet.index(letter).nil? }
+  end
+  sorted
 end
